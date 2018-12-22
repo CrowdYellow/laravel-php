2d509a1746956bd68ae60a56afe8a0cb3b11301d
echo61546@gmail.com <echo61546@gmail.com>
2018-12-22 12:14:21 +0800

    style

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -27,7 +27,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
@@ -65,7 +65,10 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
+            'photos' => "/images/default.jpg",
+            'confirmation_token' => str_random(40),
             'password' => bcrypt($data['password']),
+            'api_token' => str_random(60),
         ]);
     }
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,21 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('name')->unique();
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('confirmation_token');
+            $table->smallInteger('is_active')->default(0);
+            $table->string('photos');
+            $table->integer('questions_count')->default(0);
+            $table->integer('answers_count')->default(0);
+            $table->integer('comments_count')->default(0);
+            $table->integer('favorites_count')->default(0);
+            $table->integer('likes_count')->default(0);
+            $table->integer('followers_count')->default(0);
+            $table->integer('followings_count')->default(0);
+            $table->string('api_token',64)->unique();
+            $table->string('settings')->nullable($value = true);
             $table->rememberToken();
             $table->timestamps();
         });
